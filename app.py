a2b5da4bd8be5299fa6f4f23189855840cf8fc56
mensi911 <nk605787@gmail.com>
2025-03-29 15:12:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def generate_qr():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))  # Render использует свою переменную PORT
+    port = int(os.environ.get("PORT", 5000))  # Берём PORT из переменных окружения
     serve(app, host="0.0.0.0", port=port)
